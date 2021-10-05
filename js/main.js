4e7773c9a25911b73bc21add207d61d54a5d1572
ayumi_miyazaki <a.miyazaki313@gmail.com>
2021-10-05 14:58:21 +0900

    [Update]ハンバーガーメニュー修正（修正点あり:ボタンのクリックアクションが逆になっている） #9

--- js/main.js
+++ js/main.js
@@ -1,23 +1,41 @@
-$(function() {
-  $('#burger-btn').on('click', function(){
-    $('.icon').toggleClass('close');
-    $('.sp').slideToggle();
-  });
-});
+// $(function() {
+//   $('#burger-btn').on('click', function(){
+//     $('.icon').toggleClass('active');
+//     $('.sp').slideToggle();
+//   });
+// });
 
 
 
 
 // $(function() {
 //   $('#burger-btn').click(function() {
-//       $('.icon').toggleClass('close');
+//       $('.icon').toggleClass('active');
 
-//       if ($('.icon').hasClass('close')) {
-//           // $('.sm').addClass('close');
+//       if ($('.icon').hasClass('active')) {
+//           // $('.sm').addClass('active');
 //           $('.sm').slideToggle();
 //       } else {
-//           $('.sm').removeClass('close');
+//           $('.sm').removeClass('active');
 //       }
 //   });
 // });
 
+// $(function() {
+//   $('#burger-btn').click(function() {
+//     $(this).toggleClass('active');
+//     if ($(this).hasClass('active')) {
+//       $('.sp').addClass('active');
+//     } else {
+//       $('.sp').removeClass('active');
+//     }
+//   });
+// });
+
+$(function() {
+  $('#burger-btn').on('click', function(){
+    $(this).toggleClass('active');
+    $('.sp').slideToggle();
+    $('.sp').removeClass('active');
+  });
+});
